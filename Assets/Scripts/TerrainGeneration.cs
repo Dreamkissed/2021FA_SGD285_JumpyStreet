edde3a99fcf8f0a1163c31003531479bc04fc866
Dreamkissed <JennJP65@yahoo.com>
2021-09-08 16:35:16 -0400

    Class End Push
    9/8/2021 End of Class Time Push

--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+//////////////////////////////////////////////
+//Project: Jumpy Street
+//Name: Jennifer Wenner
+//Section: 2021FA.SGD.285.2141
+//Instructor: Prof. Wold
+//Date: 09/08/2021
+/////////////////////////////////////////////
+
 using UnityEngine;
 
 public class TerrainGeneration : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject tile;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            float tempTileX = tile.transform.position.x + 5.0f;
+            Vector3 tempTilePos = new Vector3(tempTileX, tile.transform.position.y, tile.transform.position.z);
+            tile.transform.position = tempTilePos;
+        }
     }
 }
