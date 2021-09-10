8e6beea78440d1160b354f0c5ab9290f17d7eb6c
Dreamkissed <JennJP65@yahoo.com>
2021-09-10 01:01:43 -0400

    Terrain Generation Progress
    Built Terrain Generator, terrain works as 'treadmill', and generates new tiles ahead of itself.
    
    Also per-row biome randomness, currently only three (Grass, Road, Water) but can support any number of biomes.
    
    I also inadvertently wrote the scripts that it could be expanded to treadmill in any orthogonal direction...

--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -6,24 +6,209 @@
 //Date: 09/08/2021
 /////////////////////////////////////////////
 
+using System.Collections;
 using UnityEngine;
 
 public class TerrainGeneration : MonoBehaviour
 {
-    [SerializeField] GameObject tile;
-    
-    void Start()
+    [SerializeField] private GameObject PH_tile;
+    [SerializeField] private GameObject pfb_BlankTile;
+    [SerializeField] private GameObject pfb_GrassTile;
+    [SerializeField] private GameObject pfb_RoadTile;
+    [SerializeField] private GameObject pfb_WaterTile;
+    [SerializeField] private int biomeCount = 3;
+
+    [SerializeField] private int map_lengthForward = 7;  //tiles ahead of player
+    [SerializeField] private int map_lengthBack = 3;  //tiles behind player
+    [SerializeField] private int map_widthHalf = 5;  //tiles on either side of middle column.
+    private GameObject[,] map_arrayTiles;  //set up in MapGeneration();
+    private int map_lengthTotal, map_widthTotal;  //set up in MapGeneration();
+    private int map_backRow = 0;
+    private readonly float map_tileInterval = 5.0f;  //edge length of square tiles
+    private bool map_isMoving = false;
+
+    private void Awake()
+    {
+        MapGeneration();
+    }
+
+    private void Start()
     {
         
     }
 
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            float tempTileX = tile.transform.position.x + 5.0f;
-            Vector3 tempTilePos = new Vector3(tempTileX, tile.transform.position.y, tile.transform.position.z);
-            tile.transform.position = tempTilePos;
+            float tempTileX = PH_tile.transform.position.x - map_tileInterval;
+            Vector3 tempTilePos = new Vector3(tempTileX, PH_tile.transform.position.y, PH_tile.transform.position.z);
+            PH_tile.transform.position = tempTilePos;
+
+            //StartCoroutine(TreadmillBackward());
         }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            float tempTileX = PH_tile.transform.position.x + map_tileInterval;
+            Vector3 tempTilePos = new Vector3(tempTileX, PH_tile.transform.position.y, PH_tile.transform.position.z);
+            PH_tile.transform.position = tempTilePos;
+
+            if (!map_isMoving)
+            {
+                map_isMoving = true;
+                StartCoroutine(TreadmillForward());
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            float tempTileZ = PH_tile.transform.position.z + map_tileInterval;
+            Vector3 tempTilePos = new Vector3(PH_tile.transform.position.x, PH_tile.transform.position.y, tempTileZ);
+            PH_tile.transform.position = tempTilePos;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            float tempTileZ = PH_tile.transform.position.z - map_tileInterval;
+            Vector3 tempTilePos = new Vector3(PH_tile.transform.position.x, PH_tile.transform.position.y, tempTileZ);
+            PH_tile.transform.position = tempTilePos;
+        }
+    }
+
+    private bool MapGeneration()
+    {
+        map_lengthTotal = map_lengthForward + 1 + map_lengthBack;
+        map_widthTotal = map_widthHalf * 2 + 1;
+        map_arrayTiles = new GameObject[map_lengthTotal, map_widthTotal];
+
+        for (int l = 0; l < map_lengthTotal; l++)
+        {
+            GameObject rowBiome = BiomeRandomBiomeRow();
+            for (int w = 0; w < map_widthTotal; w++)
+            {
+                float temp_tilePosX = ((float)l - (float)map_lengthBack) * map_tileInterval;
+                float temp_tilePosY = 0.0f;  //playfield base level
+                float temp_tilePosZ = ((float)w - (float)map_widthHalf) * map_tileInterval;
+                Vector3 temp_tilePos = new Vector3(temp_tilePosX, temp_tilePosY, temp_tilePosZ);
+
+                map_arrayTiles[l, w] = Instantiate(rowBiome, temp_tilePos, Quaternion.identity);
+            }
+        }
+
+        return true;
+    }
+
+    private bool MapNewRowFront()
+    {
+        GameObject rowBiome = BiomeRandomBiomeRow();
+
+        for (int w = 0; w < map_widthTotal; w++)
+        {
+            Destroy(map_arrayTiles[map_backRow, w]);
+            
+            float temp_tilePosX = (float)map_lengthForward * map_tileInterval;
+            float temp_tilePosY = 0.0f;  //playfield base level
+            float temp_tilePosZ = ((float)w - (float)map_widthHalf) * map_tileInterval;
+            Vector3 temp_tilePos = new Vector3(temp_tilePosX, temp_tilePosY, temp_tilePosZ);
+
+            map_arrayTiles[map_backRow, w] = Instantiate(rowBiome, temp_tilePos, Quaternion.identity);
+            /*
+            tempPositionVector = map_arrayTiles[map_backRow, w].transform.position;
+            tempPositionVector.x = map_lengthForward * map_tileInterval;
+            map_arrayTiles[map_backRow, w].transform.position = tempPositionVector;
+            */
+        }
+        map_backRow++;
+        map_backRow %= map_lengthTotal;
+
+        return true;
+    }
+
+    private GameObject BiomeRandomBiomeRow()
+    {
+        int randomBiome = Random.Range(0, biomeCount);
+        return randomBiome switch
+        {
+            0 => pfb_GrassTile,
+            1 => pfb_RoadTile,
+            2 => pfb_WaterTile,
+            _ => pfb_BlankTile,
+        };
+    }
+
+    private bool MapCleanFloatErrors()  //I REALLY HATE FLOATS!!!
+    {
+        for (int l = 0; l < map_lengthTotal; l++)
+        {
+            for (int w = 0; w < map_widthTotal; w++)
+            {
+                Vector3 temp_tilePos = map_arrayTiles[l, w].transform.position;
+                temp_tilePos.x = Mathf.Round(temp_tilePos.x);  //I hate floats...
+                temp_tilePos.y = Mathf.Round(temp_tilePos.y);
+                temp_tilePos.z = Mathf.Round(temp_tilePos.z);
+                
+                map_arrayTiles[l, w].transform.position = temp_tilePos;
+            }
+        }
+        return true;
+    }
+
+    private IEnumerator TreadmillForward()
+    {
+        float intervalStep = 0.1f; //!!!!!
+        
+        for (float f = 0.0f; f <= map_tileInterval; f += intervalStep)
+        {
+            for (int l = 0; l < map_lengthTotal; l++)
+            {
+                for (int w = 0; w < map_widthTotal; w++)
+                {
+                    Vector3 temp_tilePos = map_arrayTiles[l, w].transform.position;
+                    temp_tilePos.x -= intervalStep;
+                    map_arrayTiles[l, w].transform.position = temp_tilePos;
+                }
+            }
+            yield return null;
+        }
+        MapCleanFloatErrors();
+        MapNewRowFront();
+        map_isMoving = false;
+    }
+
+    private IEnumerator TreadmillBackward()
+    {
+        float intervalStep = 0.1f; //!!!!!
+
+        for (float f = 0.0f; f <= map_tileInterval; f += intervalStep)
+        {
+            for (int l = 0; l < map_lengthTotal; l++)
+            {
+                for (int w = 0; w < map_widthTotal; w++)
+                {
+                    Vector3 temp_tilePos = map_arrayTiles[l, w].transform.position;
+                    temp_tilePos.x += intervalStep;
+                    map_arrayTiles[l, w].transform.position = temp_tilePos;
+                }
+            }
+            yield return null;
+        }
+        MapCleanFloatErrors();
+        map_isMoving = true;
+    }
+
+    public bool Map_MoveForward(int backstepCount)
+    {
+        if (backstepCount <= 0)
+        {
+            StartCoroutine(TreadmillForward());
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool Map_MoveBackwards(int backstepCount)
+    {
+        return true;
     }
 }
