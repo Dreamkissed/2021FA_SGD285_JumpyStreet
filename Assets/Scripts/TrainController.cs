5202d3780bccea8778671e47e5beede1e5660c5e
Dreamkissed <JennJP65@yahoo.com>
2021-10-20 14:59:12 -0400

    QUIT BEING GITHUB!
    I swear to Lilith if Github deletes these files again, I'm gonna reach through the ethernet and strangle the server node!

---
+++ Assets/Scripts/TrainController.cs
@@ -0,0 +1,26 @@
+//////////////////////////////////////////////
+//Project: Jumpy Street
+//Name: Jennifer Wenner
+//Section: 2021FA.SGD.285.2141
+//Instructor: Prof. Wold
+//Date: 10/20/2021
+/////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
